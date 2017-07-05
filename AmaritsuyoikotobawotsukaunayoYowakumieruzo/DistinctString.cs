55bd22130d2895f7424136bd2797d7b719992574
うぃんじー <wing3196@gmail.com>
2017-07-05 10:04:16 +0900

    ReplaceTestの投げる例外がおかしい(要調査)

--- AmaritsuyoikotobawotsukaunayoYowakumieruzo/DistinctString.cs
+++ AmaritsuyoikotobawotsukaunayoYowakumieruzo/DistinctString.cs
@@ -6,32 +6,40 @@ using System.Threading.Tasks;
 
 namespace AmaritsuyoikotobawotsukaunayoYowakumieruzo
 {
-    class DistinctString
+    public class DistinctString
     {
         public string Str { get; private set; }
         public List<int> Ids { get; private set; }
+        private DistinctString(string str, IEnumerable<int> ids)
+        {
+            Str = str;
+            Ids = ids.ToList();
+        }
         public DistinctString(string str)
         {
             Str = str;
             Ids = new List<int>(str.Count());
             for (int i = 0; i < str.Count(); i++) Ids.Add(i);
         }
-        public void Delete(int index, int count)
+        public DistinctString Delete(int index, int count)
         {
-            Str = Str.Substring(0, index) + Str.Substring(index + count);
-            Ids = (List<int>)Enumerable.Concat(
+            string str = Str.Substring(0, index) + Str.Substring(index + count);
+            List<int> ids = Enumerable.Concat(
                 Ids.GetRange(0, index),
-                Ids.GetRange(index + count, Ids.Count - index - count));
+                Ids.GetRange(index + count, Ids.Count - index - count)
+                ).ToList();
+            return new DistinctString(str, ids);
         }
-        public void Replace(int index, int count, string target)
+        public DistinctString Replace(int index, int count, string replaced)
         {
-            Str = Str.Substring(0, index) + target + Str.Substring(index + count);
-            Ids = (List<int>)Enumerable.Concat(
+            string str = Str.Substring(0, index) + replaced + Str.Substring(index + count);
+            List<int> ids = Enumerable.Concat(
                 Ids.GetRange(0, index),
                 Enumerable.Concat(
-                    Enumerable.Repeat(-1, target.Count()),
+                    Enumerable.Repeat(-1, replaced.Count()),
                     Ids.GetRange(index + count, Ids.Count - index - count)
-                ));
+                )).ToList();
+            return new DistinctString(str, ids);
         }
     }
 }
---
+++ AmaritsuyoikotobawotsukaunayoYowakumieruzoTests/DistinctStringTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AmaritsuyoikotobawotsukaunayoYowakumieruzo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmaritsuyoikotobawotsukaunayoYowakumieruzo.Tests
+{
+    [TestClass()]
+    public class DistinctStringTests
+    {
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            DistinctString dstr = new DistinctString("0123456789");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; i + j < 10; j++)
+                {
+                    string expected = "";
+                    for (int k = 0; k < i; k++) expected += k.ToString();
+                    for (int k = i + j; k < 10; k++) expected += k.ToString();
+                    DistinctString ret = dstr.Delete(i, j);
+                    Console.WriteLine(expected + " " + ret.Str);
+                    Assert.AreEqual(expected, ret.Str);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void ReplaceTest()
+        {
+            Random rand = new Random();
+            DistinctString dstr = new DistinctString("0123456789");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; i + j < 10; j++)
+                {
+                    string expectedStr = "";
+                    List<int> expectedIds = new List<int>();
+                    for (int k = 0; k < i; k++) { expectedStr += k.ToString(); expectedIds.Add(k); }
+                    IEnumerable<Char> randChars = (new string('a', rand.Next(15))).Select((c, p) => (char)(c + rand.Next(26)));
+                    string replaced = string.Join("", randChars);
+                    expectedStr += replaced;
+                    for (int k = 0; k < replaced.Length; k++) expectedIds.Add(-1);
+                    for (int k = i + j; k < 10; k++) { expectedStr += k.ToString(); expectedIds.Add(k); }
+                    DistinctString ret = dstr.Replace(i, j, replaced);
+                    Console.WriteLine(expectedStr + " " + ret.Str);
+                    Console.WriteLine("\t" + expectedIds + " " + ret.Ids);
+                    Assert.AreEqual(expectedStr, ret.Str);
+                    Assert.AreEqual(expectedIds, ret.Ids);
+                }
+            }
+        }
+    }
+}
