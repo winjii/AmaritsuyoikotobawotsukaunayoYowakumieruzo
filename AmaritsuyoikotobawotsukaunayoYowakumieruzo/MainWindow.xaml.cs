c8fe179d073de22983bf31a4bd76f694d9e45af2
うぃんじー <wing3196@gmail.com>
2017-07-07 01:56:35 +0900

    アニメーションを実装(バグだらけ)

--- AmaritsuyoikotobawotsukaunayoYowakumieruzo/MainWindow.xaml.cs
+++ AmaritsuyoikotobawotsukaunayoYowakumieruzo/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,45 +21,87 @@ namespace AmaritsuyoikotobawotsukaunayoYowakumieruzo
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<TextBox> chars;
-        double fontSize = 0;
+        private List<TextBox> chars;
+        private KeyTime animationTime;
         public MainWindow()
         {
             InitializeComponent();
-            fontSize = textBox.FontSize;
-            FontFamily font = textBox.FontFamily;
-            string str = "TextBoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-            textBox.Text = str;
             textBox.TextWrapping = TextWrapping.Wrap;
-            chars = new List<TextBox>();
-            Canvas.SetLeft(textBox, 0);
-            Canvas.SetTop(textBox, 0);
+            animationTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(1500));
+        }
+
+        private double getCharSize(char target)
+        {
+            int d = Util.IsHalfByRegex(target) ? 2 : 1;
+            return textBox.FontSize/d;
+        }
+
+        private List<Point> getPos(string str)
+        {
+            List<Point> res = new List<Point>();
+            double x = 0, y = 0;
             for (int i = 0; i < str.Length; i++)
             {
+                res.Add(new Point(x, y));
+                x += getCharSize(str[i]);
+                if (x > textBox.ActualWidth)
+                {
+                    x = 0;
+                    y += textBox.FontSize;
+                }
+            }
+            return res;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DistinctString pastStr = new DistinctString(textBox.Text);
+            StringRebuilder rebuilder = new StringRebuilder(pastStr);
+            Random rand = new Random();
+            int p = rand.Next(pastStr.Str.Length), cnt = rand.Next(pastStr.Str.Length - p) + 1;
+            rebuilder.ReserveDeletion(p, cnt);
+            rebuilder.ReserveAddition(p, "lattemaltamalta");
+            DistinctString newStr = rebuilder.Rebuild();
+
+            List<Point> pastPos = getPos(pastStr.Str);
+            List<Point> newPos = getPos(newStr.Str);
+            textBox.Text = "";
+            chars = new List<TextBox>();
+            for (int i = 0; i < pastStr.Str.Length; i++)
+            {
                 TextBox charBox = new TextBox();
-                charBox.FontFamily = font;
-                charBox.FontSize = fontSize;
-                charBox.Text = str[i].ToString();
+                charBox.FontFamily = textBox.FontFamily;
+                charBox.Text = pastStr.Str[i].ToString();
                 charBox.BorderThickness = new Thickness(0);
                 charBox.IsReadOnly = true;
                 chars.Add(charBox);
                 canvas.Children.Add(charBox);
-            }
-        }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            textBox.MaxWidth = canvas.ActualWidth;
-            double x = 0, y = 200;
-            foreach (TextBox charBox in chars)
-            {
-                Canvas.SetLeft(charBox, x);
-                Canvas.SetTop(charBox, y);
-                x += fontSize/2;
-                if (x > canvas.ActualWidth)
-                {
-                    y += fontSize;
-                    x = 0;
-                }
+                Canvas.SetLeft(charBox, pastPos[i].X);
+                Canvas.SetTop(charBox, pastPos[i].Y);
+
+                DoubleKeyFrame frameX = new EasingDoubleKeyFrame(newPos[i].X, animationTime);
+                DoubleAnimationUsingKeyFrames animationX = new DoubleAnimationUsingKeyFrames();
+                animationX.KeyFrames.Add(frameX);
+                Storyboard storyboardX = new Storyboard();
+                storyboardX.Children.Add(animationX);
+                Storyboard.SetTarget(animationX, charBox);
+                Storyboard.SetTargetProperty(animationX, new PropertyPath("(Canvas.Left)"));
+
+                DoubleKeyFrame frameY = new EasingDoubleKeyFrame(newPos[i].Y, animationTime);
+                DoubleAnimationUsingKeyFrames animationY = new DoubleAnimationUsingKeyFrames();
+                animationY.KeyFrames.Add(frameY);
+                Storyboard storyboardY = new Storyboard();
+                storyboardY.Children.Add(animationY);
+                Storyboard.SetTarget(animationY, charBox);
+                Storyboard.SetTargetProperty(animationY, new PropertyPath("(Canvas.Top)"));
+
+                //storyboardX.Begin();
+                //storyboardY.Begin();
             }
         }
     }
---
+++ AmaritsuyoikotobawotsukaunayoYowakumieruzo/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AmaritsuyoikotobawotsukaunayoYowakumieruzo
+{
+    static class Util
+    {
+        public static bool IsHalfByRegex(char target)
+        {
+            return new Regex("^[\u0020-\u007E\uFF66-\uFF9F]+$").IsMatch(target.ToString());
+        }
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
